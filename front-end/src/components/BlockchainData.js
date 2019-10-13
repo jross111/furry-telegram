a5e5a6b33ccd70deabf21b72f9dcd87537078b57
jross111 <jross.nyc111@gmail.com>
2019-10-13 18:23:34 -0400

    :boom:

--- front-end/src/components/BlockchainData.js
+++ front-end/src/components/BlockchainData.js
@@ -11,9 +11,7 @@ export default class BlockchainData extends Component {
             .then(res => res.json())
             .then(res => this.setState({ data: res }))
 
-
     }
-
     render() {
 
 
