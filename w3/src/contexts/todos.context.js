be2d1b0f37f59dc364c1d4527fc3df57f7207f2e
jross111 <jross.nyc111@gmail.com>
2019-11-03 10:06:39 -0500

    :fire:

--- w3/src/contexts/todos.context.js
+++ w3/src/contexts/todos.context.js
@@ -1,21 +1,27 @@
 import React, { createContext, useReducer } from 'react';
 import todosReducer from '../reducers/todos.reducer';
 
-
 const defaultTodos = [
-    { id: '0', task: 'violin', completed: false},
-    { id: '1', task: 'something else', completed: true},
-    { id: '2', task: 'make some food', completed: false},
+    { id: '0', task: 'violin', completed: false },
+    { id: '1', task: 'something else', completed: true },
+    { id: '2', task: 'make some food', completed: false },
 ];
 
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
+
 
 export function TodosProvider(props) {
-    const [todos] = useReducer(todosReducer, defaultTodos);
+
+    const [todos, dispatch] = useReducer(todosReducer, defaultTodos);
 
     return (
         <TodosContext.Provider value={todos}>
-            {props.children}
+            <DispatchContext.Provider value={dispatch}>
+                {props.children}
+            </DispatchContext.Provider>
         </TodosContext.Provider>
     )
 }
+
+
